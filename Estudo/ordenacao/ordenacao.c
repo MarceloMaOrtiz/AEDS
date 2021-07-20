fc768813d137989233439c9a80866631599d1b49
MOMachine <marcelomaortiz@hotmail.com>
2021-07-19 21:52:46 -0300

    Correçao no ShellSort no Estudo, e adiçao do Merge e Quick Sort

--- Estudo/ordenacao/ordenacao.c
+++ Estudo/ordenacao/ordenacao.c
@@ -16,8 +16,7 @@
 
 #include "stdio.h"
 #include "stdlib.h"
-// #include "string.h"
-// #include "aluno.h"
+#include "math.h"
 
 // BubbleSort
 //  - Compara pares de elementos adjacentes e os troca de lugar se estiverem na ordem errada
@@ -108,18 +107,114 @@ void shell_sort(int *v, int tam){
     }while(h < tam);
     do{
         h = h / 3;
-        for(i = h+1; i <= tam; i++){
+        for(i = h; i < tam; i++){
             x = v[i];
-            j = i;
-            while(v[j-h] > x){
-                v[j] == v[j-h];
+            j = i - h;
+            while(j >= 0 && v[j] > x){
+                v[j+h] = v[j];
                 j = j - h;
-                if(j <= h)
-                    break;
             }
-            v[j] = x;
+            v[j+h] = x;
         }
-    }while(h != 1);
+    }while(h > 1);
+}
+
+// MergeSort
+//  - Ideia Básica
+//      - Dividir e Conquistar
+//          - Divide, recursivamente, o conjunto de dados até que cada subconjunto
+//              possua 1 elemento
+//          - Combina 2 subconjuntos de forma a obter 1 subconjunto maior e ordenado
+//          - Processo se repete até existir 1 conjunto
+//  - Performance
+//      - Melhor Caso: O(n*log(n))
+//      - Pior Caso: O(n*log(n))
+//      - Estável: Não altera a ordem de dados iguais
+//      - Desvatagens
+//          - Recursivo
+//          - Usa um vetor auxiliar durante a ordenação
+void merge(int *v, int inicio, int meio, int fim){
+    int *temp, p1, p2, tamanho, i, j, k;
+    int fim1 = 0, fim2 = 0.;
+    tamanho = fim - inicio + 1;
+    p1 = inicio;
+    p2 = meio + 1;
+    temp = (int*)malloc(tamanho*sizeof(int));
+    if(temp != NULL){
+        for(i = 0; i < tamanho; i++){
+            if(!fim1 && !fim2){
+                if(v[p1] < v[p2])
+                    temp[i] = v[p1++];
+                else
+                    temp[i] = v[p2++];
+                if(p1 > meio)
+                    fim1 = 1;
+                if(p2 > fim)
+                    fim2 = 1;
+            }else{
+                if(!fim1)
+                    temp[i] = v[p1++];
+                else
+                    temp[i] = v[p2++];
+            }
+        }
+        for(j = 0, k = inicio; j < tamanho; j++, k++)
+            v[k] = temp[j];
+    }
+    free(temp);
+}
+
+void merge_sort(int *v, int inicio, int fim){
+    int meio;
+    if(inicio < fim){
+        meio = floor((inicio+fim)/2);
+        merge_sort(v, inicio, meio);
+        merge_sort(v, meio+1, fim);
+        merge(v, inicio, meio, fim);
+    }
+}
+
+// QuickSort
+//  - Ideia Básica
+//      - Dividir e Conquistar
+//      - Um elemento é escolhido como pivô
+//      - O dados são rearranjados
+//          - Valores menores que o pivô são colocados antes dele e os maiores, depois
+//      - recursivamente ordena as duas partições
+//  - Performance
+//      - Melhor Caso: O(n*log(n))
+//      - Pior Caso: O(n²)
+//      - Estável: Não altera a ordem de dados iguais
+//      - Desvantagens
+//          - Como escolher o pivô?
+int particiona(int *v, int inicio, int final){
+    int esq, dir, pivo, aux;
+    esq = inicio;
+    dir = final;
+    pivo = v[inicio];
+    while(esq < dir){
+        while(v[esq] <= pivo)
+            esq++;
+        while(v[dir] > pivo)
+            dir --;
+        if(esq < dir){
+            aux = v[esq];
+            v[esq] = v[dir];
+            v[dir] = aux;
+        }
+    }
+    v[inicio] = v[dir];
+    v[dir] = pivo;
+    return dir;
+}
+
+void quick_sort(int *v, int inicio, int fim){
+    int pivo;
+    if(fim > inicio){
+        pivo = particiona(v, inicio, fim);
+        quick_sort(v, inicio, pivo-1);
+        quick_sort(v, pivo+1, fim);
+    }
 }
 
 void imprimir(int *v, int tam){
@@ -153,8 +248,20 @@ int main(){
     int vetor_shell[10] = {4, 5, 3, 2, 1, 9, 7, 0, 8, 6};
     printf("\n - ShellSort\n");
     imprimir(vetor_shell, 10);
-    selection_sort(vetor_shell, 10);
+    shell_sort(vetor_shell, 10);
     imprimir(vetor_shell, 10);
+
+    int vetor_merge[10] = {4, 5, 3, 2, 1, 9, 7, 0, 8, 6};
+    printf("\n - MergeSort\n");
+    imprimir(vetor_merge, 10);
+    merge_sort(vetor_merge, 0, 9);
+    imprimir(vetor_merge, 10);
+
+    int vetor_quick[10] = {4, 5, 3, 2, 1, 9, 7, 0, 8, 6};
+    printf("\n - QuickSort\n");
+    imprimir(vetor_quick, 10);
+    quick_sort(vetor_quick, 0, 9);
+    imprimir(vetor_quick, 10);
 
     return 0;
 }
